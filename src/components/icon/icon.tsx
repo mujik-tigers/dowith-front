5b8e33b98c9a8f0b80cae8d532281e2cf42eb833
poco <godiva140@gmail.com>
2024-08-06 14:46:37 +0900

    feat: 카카오 로그인 기능 구현

---
+++ src/components/icon/icon.tsx
@@ -0,0 +1,13 @@
+// import IconComponents from '@/assets/icons';
+
+// interface IIconProps {
+//   className?: string;
+//   name: keyof typeof IconComponents;
+// }
+
+// const Icon = ({ className, name }: IIconProps) => {
+//   const IconComponent = IconComponents[name];
+//   return <IconComponent className={className} />;
+// };
+
+// export default Icon;
