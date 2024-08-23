ff47049aab3c3b28a3205c1747307484ac493598
poco <godiva140@gmail.com>
2024-08-23 14:12:05 +0900

    chore: zustand 최신 버전으로 업데이트 및 svg 파일 컴포넌트화 사용을 위한 vite-plugin-svgr 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
