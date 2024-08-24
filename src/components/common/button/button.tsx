8125ff82339fdc3f87e3f75f965c36a8e999612b
WK <mwk0725@gmail.com>
2024-08-24 23:25:57 +0900

    refactor: button 리팩토링
    - 버튼 type, motion, style 수정

--- src/components/common/button/button.tsx
+++ src/components/common/button/button.tsx
@@ -1,4 +1,5 @@
 import { Button as UIButton } from '@/components/ui/button';
+import { motion } from 'framer-motion';
 
 const SIZE_CLASSES = {
   fixedS: 'h-[36px] w-[72px] rounded-md',
@@ -11,8 +12,8 @@ const SIZE_CLASSES = {
 const BG_COLOR_CLASSES = {
   red: 'bg-red hover:bg-red/90 text-white',
   yellow: 'bg-yellow hover:bg-yellow/90 text-text',
-  white: 'bg-white text-text',
-  black: 'bg-black text-white',
+  white: 'bg-white text-text hover:bg-white/90 border border-line',
+  black: 'bg-black text-white hover:bg-black/90',
   blue: 'bg-blueLight hover:bg-blueLight/90 text-text',
 } as const;
 
@@ -21,20 +22,26 @@ export const Button: React.FC<{
   className?: string;
   size?: keyof typeof SIZE_CLASSES;
   bgColor?: keyof typeof BG_COLOR_CLASSES;
-  onClick: () => void;
+  onClick?: () => void;
+  type?: 'button' | 'submit';
 }> = ({
   children,
-  className,
+  type = 'button',
   size = 'flexibleM',
   bgColor = 'red',
   onClick,
 }) => {
+  const MotionButton = motion(UIButton);
+
   return (
-    <UIButton
+    <MotionButton
+      type={type}
+      initial={{ scale: 1 }}
+      whileTap={{ scale: 0.97 }}
       onClick={onClick}
-      className={`${SIZE_CLASSES[size]} ${BG_COLOR_CLASSES[bgColor]} ${className}`}
+      className={`${SIZE_CLASSES[size]} ${BG_COLOR_CLASSES[bgColor]}`}
     >
       {children}
-    </UIButton>
+    </MotionButton>
   );
 };
