a328ab393b7fa593e93b785e3c7f0c061c6245f1
poco <godiva140@gmail.com>
2024-09-10 17:13:19 +0900

    feat: tooltip 스토리북 작성

---
+++ src/components/common/tooltip/tooltip.stories.tsx
@@ -0,0 +1,48 @@
+import { Tooltip } from '@/components/common/tooltip/tooltip';
+import { Meta, StoryFn } from '@storybook/react';
+import React from 'react';
+
+export default {
+  title: 'components/common/Tooltip',
+  component: Tooltip,
+  argTypes: {
+    triggerStyle: {
+      control: 'select',
+      options: ['rounded'],
+    },
+    contentStyle: {
+      control: 'select',
+      options: ['default'],
+    },
+    contentTextStyle: {
+      control: 'select',
+      options: ['default'],
+    },
+    tooltipLocation: {
+      control: 'select',
+      options: ['top', 'bottom', 'left', 'right'],
+    },
+    delayDuration: {
+      control: 'number',
+    },
+    sideOffset: {
+      control: 'number',
+    },
+  },
+} as Meta;
+
+const Template: StoryFn<React.ComponentProps<typeof Tooltip>> = (args) => (
+  <Tooltip {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  trigger: <button>tooltip</button>,
+  content: 'This is a tooltip',
+  triggerStyle: 'rounded',
+  contentStyle: 'default',
+  contentTextStyle: 'default',
+  tooltipLocation: 'top',
+  delayDuration: 300,
+  sideOffset: 10,
+};
