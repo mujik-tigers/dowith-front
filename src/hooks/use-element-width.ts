53f852e7753be15dc70d42e72bd4eecaf88d59d6
poco <godiva140@gmail.com>
2024-11-26 13:53:12 +0900

    feat: useElementWidth 디바운스 처리 추가

--- src/hooks/use-element-width.ts
+++ src/hooks/use-element-width.ts
@@ -1,5 +1,21 @@
 import { useState, useEffect, useRef } from 'react';
 
+type DebounceSetWidthCallback<T> = (value: T) => void;
+
+const debounceSetWidth = <T>(
+  callback: DebounceSetWidthCallback<T>,
+  timeout = 300
+) => {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+
+  return (value: T) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(value);
+    }, timeout);
+  };
+};
+
 export const useElementWidth = <T extends HTMLElement>(isActive?: boolean) => {
   const elementRef = useRef<T>(null);
   const [width, setWidth] = useState<number>(0);
@@ -7,12 +23,14 @@ export const useElementWidth = <T extends HTMLElement>(isActive?: boolean) => {
   useEffect(() => {
     if (!elementRef.current) return;
 
+    const debouncedSetWidth = debounceSetWidth(setWidth, 500);
+
     setWidth(elementRef.current.clientWidth);
 
     const observer = new ResizeObserver((entries) => {
       for (const entry of entries) {
         if (entry.contentRect) {
-          setWidth(entry.contentRect.width);
+          debouncedSetWidth(entry.contentRect.width);
         }
       }
     });
